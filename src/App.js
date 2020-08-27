70db184fd3c114c2df666abeb63d00bd3a6a1283
JohnDusang <Johndusang1@gmail.com>
2020-08-26 22:39:43 -0600

    trying to fix hosting errors

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EmployeeContainer from './components/EmployeeContainer';
+
+
+function App() {
+  return (
+    <div>
+      <EmployeeContainer />
+    </div >
+  )
+
+};
+
+export default App;
---
+++ src/components/TitleCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+// Will Show at top of every site 
+function TitleCard(props) {
+    return (
+        <div className="card-centered">
+            <div className="card-body text-center">
+                Employee Registry
+            </div>
+        </div>
+    );
+}
+
+export default TitleCard;
+
